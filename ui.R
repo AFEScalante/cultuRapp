d35893ad6a98516bbebd0e43cb382c62306a91b9
Escalante <Angel.Escalante@kantar.com>
2022-05-06 10:37:44 -0500

    Ajuste en tamaño y titulos en la UI

--- ui.R
+++ ui.R
@@ -14,19 +14,20 @@ load("datos/puntos-culturales-cdmx.RData")
 lugares <- puntos_tbl$lugar
 
 ui <- fluidPage(
-  title = "Shiny - cultuRa CDMX",
+  title = "Shiny App - cultuRapp CDMX",
   setBackgroundColor("#171717"),
   column(
     br(), br(), br(),
     div(style="display: inline-block;vertical-align:left; width: 130px;
         color: white;",
         br(),
-        h2("cultuRa CDMX"), align = "left"),
+        h2("cultuRapp CDMX"), align = "left"),
     br(), br(),
     div(style="color: gray;",
         h3("Puntos culturales dentro de la Ciudad de México")),
     div(style="color: gray; width: 335px; align: left; margin-left: 50px;",
-        p(align = "left", "Esta aplicación muestra un mapa con todos los puntos culturales según el SIC México (Sistema de Información Cultural) extraído al 20/12/2021.")),
+        p(align = "left", 
+          "Esta aplicación muestra un mapa con todos los puntos culturales según el SIC México (Sistema de Información Cultural) extraído al 20/12/2021.")),
     br(), br(),
     selectizeInput(inputId = "tipo",
                    label = "Tipo de lugar",
@@ -42,8 +43,8 @@ ui <- fluidPage(
     align = "center", width = 4
   ),
   column(
-    leafletOutput("mapa", width = 930, height = 500),
-    echarts4rOutput("conteosPlot", width = 930, height = 320),
+    leafletOutput("mapa", width = 700, height = 380),
+    echarts4rOutput("conteosPlot", width = 700, height = 200),
     width = 8,
     align = "right"
   )
